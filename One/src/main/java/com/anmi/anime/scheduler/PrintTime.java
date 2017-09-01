5d5f46448af827459368513ab2c042f80685ff8b
anmi <anmi_1024@sina.com>
2017-09-01 16:09:39 +0800

    One add log

--- One/src/main/java/com/anmi/anime/scheduler/PrintTime.java
+++ One/src/main/java/com/anmi/anime/scheduler/PrintTime.java
@@ -1,5 +1,7 @@
 package com.anmi.anime.scheduler;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -11,11 +13,13 @@ import java.util.Date;
  */
 @Component
 public class PrintTime {
+    private Logger logger = Logger.getLogger(this.getClass());
 
     private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     @Scheduled(cron = "*/5 * * * * ?")
     public void reportCurrentTime(){
         System.out.println("现在时间："+sdf.format(new Date()));
+        logger.info("现在时间："+sdf.format(new Date()));
     }
 }
